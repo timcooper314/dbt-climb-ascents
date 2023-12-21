9c4d4d8d9324586ac6bba9c2df93de4d53584589
Tim Cooper <timcooper314@gmail.com>
2023-12-21 14:04:15 +1100

    Remove glue crawler. Seeds are automatically moved to s3, to create glue tables.

--- cdk/dbt_sector_stack.py
+++ cdk/dbt_sector_stack.py
@@ -32,20 +32,6 @@ class DbtSector(Stack):
             versioned=True,
         )
 
-        glue_crawler_role = iam.Role(
-            self,
-            "CrawlerRole",
-            role_name=f"{app_context}{resource_group.title().replace('-', '')}CsvSeedsCrawlerRole",
-            assumed_by=iam.ServicePrincipal(service="glue.amazonaws.com"),
-            managed_policies=[
-                iam.ManagedPolicy.from_managed_policy_arn(
-                    self,
-                    "GlueServiceRole",
-                    managed_policy_arn="arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole",
-                )
-            ],
-        )
-        raw_seeds_data_bucket.grant_read(glue_crawler_role)
         ###############################################
         #          Glue Database                      #
         ###############################################
@@ -58,40 +44,4 @@ class DbtSector(Stack):
                 name=database_name,
                 # description=f"Database for {resource_group.title().replace('-', ' ')}",
             ),
-        )
-        ###############################################
-        #          Glue Crawler                       #
-        ###############################################
-        csv_classifier = glue.CfnClassifier(
-            self,
-            "CsvClassifier",
-            csv_classifier=glue.CfnClassifier.CsvClassifierProperty(
-                name=f"{app_context}-dbt-sector-seeds-csv-classifier",
-                delimiter=",",
-                quote_symbol='"',
-                contains_header="PRESENT",
-                allow_single_column=False,
-                disable_value_trimming=False,
-            ),
-        )
-        glue_crawler = glue.CfnCrawler(
-            self,
-            "GlueCrawler",
-            name=f"{app_context}-{resource_group}-crawler",
-            role=glue_crawler_role.role_arn,
-            targets=glue.CfnCrawler.TargetsProperty(
-                s3_targets=[
-                    glue.CfnCrawler.S3TargetProperty(
-                        path=f"s3://{raw_seeds_data_bucket.bucket_name}/{s3_path}",
-                    )
-                    for s3_path in s3_targets
-                ]
-            ),
-            database_name=database_name,
-            schema_change_policy=glue.CfnCrawler.SchemaChangePolicyProperty(
-                delete_behavior="LOG", update_behavior="UPDATE_IN_DATABASE"
-            ),
-            configuration='{"Version":1.0,"CrawlerOutput":{"Partitions":{"AddOrUpdateBehavior":"InheritFromTable"},"Tables":{"AddOrUpdateBehavior":"MergeNewColumns"}}}',
-            classifiers=[f"{app_context}-dbt-sector-seeds-csv-classifier"],
-
         )
